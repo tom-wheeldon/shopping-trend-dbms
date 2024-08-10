ed3f12e84356377dd05bcf015bedf4f485964155
Tom Wheeldon <tomwheeldon@gmail.com>
2024-08-10 18:16:09 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from visualise_demographics import visualise_customer_demographics
-from visualise_seasonal_sales import visualise_seasonal_sales_trends
-from visualise_top_products import visualise_top_10_products
-
-if __name__ == "__main__":
-    visualise_customer_demographics()
-    visualise_seasonal_sales_trends()
-    visualise_top_10_products()
